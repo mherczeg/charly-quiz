aff3a9657a7a36276430169076d013815f0cc67e
Mate Herczeg <herczeg.mt@gmail.com>
2020-10-22 18:35:20 +0200

    added redux store mock for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
 import App from './App';
+import { Provider } from 'react-redux';
 
-test('renders', () => {
-    render(<App />);
-});
+let mockStore;
+
+const renderWithStore = (component, store) => {
+    render(<Provider store={store}>
+        {component}
+    </Provider>);
+}
+
+describe('<App />', () => {
+    beforeAll(() => {
+        mockStore = configureStore([]);
+    })
+
+    test('renders', () => {
+        const initialState = { quiz: {} };
+        const store = mockStore(initialState)
+        renderWithStore(<App />, store);
+    });
+
+})
